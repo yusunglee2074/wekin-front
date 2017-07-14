33995ca0822b6f738b6134180366d06e59a84b9a
yusunglee <lys0333@gmail.com>
2017-07-14 14:24:45 +0900

    이벤트 뷰 페이지 추가

---
+++ src/views/Event.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="event">
+    <div class="navbar-custom"></div>
+    <img id="about" :src=`/static/images/promotion/${imageIndex}.jpeg`/>
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      imageIndex: null
+    }
+  },
+  methods: {
+    getImageWithPageIndex() {
+      this.imageIndex = this.$route.params.key
+      console.log(this.imageIndex)
+    }
+  },
+  mounted() {
+    this.getImageWithPageIndex()
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.about {
+  text-align: center;
+
+  img {
+
+    &#about {
+      width: 100%;
+      max-width: 900px;
+    }
+    &#icon {
+      width: 18%;
+      height: 18%;
+      max-width: 100px;
+      max-height: 100px;
+    }
+  }
+}
+</style>
