027f2e2323427251457f0e7d4399c83da94bf297
yusunglee <lys0333@gmail.com>
2017-07-17 09:03:47 +0900

    빌드, 콘픽 폴더 예전으로 다 되돌림

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -18,7 +18,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       extract: true
     })
   },
-  devtool: '#nosources-source-map',
+  devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
@@ -33,7 +33,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       compress: {
         warnings: false
       },
-      sourceMap: false 
+      sourceMap: true
     }),
     // extract css into its own file
     new ExtractTextPlugin({
