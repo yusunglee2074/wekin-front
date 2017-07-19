3fbb0612817adb21a3048c121213bbd9fb851f66
yusunglee <lys0333@gmail.com>
2017-07-19 10:19:24 +0900

    회원가입시 # href 제거

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="button-container" v-if="!user">
         <a class="ui button primary" @click="showLoginModal = true">로그인</a>
-        <a class="ui button basic item linked" @click="showSignupModalMethod" href="#">회원가입</a>
+        <a class="ui button basic item linked" @click="showSignupModalMethod">회원가입</a>
       </div>
       <div class="login-info-container" v-if="user">
         <a :href="`/users/${user.user_key}`" tag="div" class="ui circular background profile image link" @click.native="toggleMobileMenu()" v-if="!isHostMode" v-bind:style="{'background-image': `url(${user.picture || user.profile_image})`}"></a>
@@ -98,7 +98,7 @@
       </div>
       <div class="right menu" v-if="!user">
         <a class="item linked" @click="showLoginModal = true">로그인</a>
-        <a class="item linked" @click="showSignupModalMethod" href="#">회원가입</a>
+        <a class="item linked" @click="showSignupModalMethod">회원가입</a>
 
         <!--<div class="loading"></div>-->
       </div>
