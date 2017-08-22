f28fe578dcd7325ba32270448681acc7622b7786
yusunglee <lys0333@gmail.com>
2017-08-22 15:02:58 +0900

    semantic css gzip cdn사용, 경량 글꼴 사용

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -103,7 +103,7 @@ if (config.build.productionGzip) {
         config.build.productionGzipExtensions.join('|') +
         ')$'
       ),
-      threshold: 10240,
+      threshold: 1024,
       minRatio: 0.8
     })
   )
