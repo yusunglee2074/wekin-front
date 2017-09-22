a04c0cd36f840e15c20b4606b2e583dff0dabdde
yusunglee2074 <lys0333@gmail.com>
2017-09-22 20:17:14 +0900

    단체활동 나브바에 프론트 삽입

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -53,8 +53,9 @@
       <router-link :to="{ name: 'Home' }" class="item link" tag="div" exact v-on:click.native="toggleMobileMenu()">위킨 홈</router-link>
       <router-link :to="{ name: 'Activity' }" class="item link" tag="div" exact v-on:click.native="toggleMobileMenu()">위킨</router-link>
       <router-link :to="{ name: 'Feed' }" class="item link" tag="div" exact v-on:click.native="toggleMobileMenu()">피드</router-link>
+      <router-link :to="{ name: 'Manual', params: { key: 2 } }" class="item linked" tag="div" exact>단체활동</router-link>
       <router-link :to="{ name: 'Customer' }" class="item link" tag="div" exact v-on:click.native="toggleMobileMenu()">고객센터</router-link>
-      <router-link :to="{ name: 'Manual' }" class="item link" tag="div" exact v-on:click.native="toggleMobileMenu()">등록방법</router-link>
+      <router-link :to="{ name: 'Manual', key: 1 }" class="item link" tag="div" exact v-on:click.native="toggleMobileMenu()">등록방법</router-link>
       <router-link :to="{ name: 'Notification' }" class="item link" tag="div" exact v-on:click.native="toggleMobileMenu()" v-if="user">알림</router-link>
       <div class="item link" v-if="user" @click="onLogoutClick()">로그아웃</div>
     </div>
@@ -75,8 +76,9 @@
     <div class="ui menu mobile-sub-nav" v-if="!isHostMode">
       <router-link :to="{ name: 'Activity' }" class="link" exact>위킨</router-link>
       <router-link :to="{ name: 'Feed' }" class="link" exact>피드</router-link>
+      <router-link :to="{ name: 'Manual', params: { key: 2 } }" class="link" exact>단체활동</router-link>
       <router-link :to="{ name: 'Customer' }" class="link" exact>고객센터</router-link>
-      <router-link :to="{ name: 'Manual' }" class="link" exact>등록방법</router-link>
+      <router-link :to="{ name: 'Manual', params: { key: 1 } }" class="link" exact>등록방법</router-link>
     </div>
   
     <!-- 모바일 끝 -->
@@ -97,8 +99,9 @@
       <div class="menu">
         <router-link :to="{ name: 'Activity' }" class="item linked" tag="div" exact>위킨</router-link>
         <router-link :to="{ name: 'Feed' }" class="item linked" tag="div" exact>피드</router-link>
+        <router-link :to="{ name: 'Manual', params: { key: 2 } }" class="item linked" tag="div" exact>단체활동</router-link>
         <router-link :to="{ name: 'Customer' }" class="item linked" tag="div" exact>고객센터</router-link>
-        <router-link :to="{ name: 'Manual' }" class="item linked" tag="div" exact>등록방법</router-link>
+        <router-link :to="{ name: 'Manual', params: { key: 1 } }" class="item linked" tag="div" exact>등록방법</router-link>
       </div>
       <div class="right menu" v-if="!user">
         <a class="item linked" @click="showLoginModal = true">로그인</a>
