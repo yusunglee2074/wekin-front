e48bf7b598e66ac3d3e6a4b6ca6e57fa676f89f2
yusunglee2074 <lys0333@gmail.com>
2017-11-03 10:05:12 +0900

    포인트 히스토리 테이블 반응형으로 변경

--- src/components/PointChangeHistory.vue
+++ src/components/PointChangeHistory.vue
@@ -1,50 +1,54 @@
 <template>
   <div class="history" style="margin-top:20px;">
 
-<div class="ui basic buttons">
-  <div class="ui button" :class="{ active: togglePointTable == 'person' }" @click="togglePointTable='person'">개인포인트</div>
-  <div class="ui button" :class="{ active: togglePointTable !== 'person' }" @click="togglePointTable='company'">기업포인트</div>
-</div>
-<!-- 개인포인트 표 -->
-    <table class="ui celled table" v-if="togglePointTable === 'person'">
-      <thead>
-        <tr>
-          <th>사용/적립일</th>
-          <th>변동양</th>
-          <th>관련 위킨</th>
-          <th>만료일</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr :class="{ negative: history.point_change < 0, positive: history.point_change > 0 }" v-for="history in historys" v-if="isPeopleType(history.type)">
-          <td>{{ history.created_at | formatDateWithMoment }}</td>
-          <td>{{ history.point_change }}</td>
-          <td >{{ history.WekinNew.ActivityNew.title }}</td>
-          <td>{{ history.due_date_be_written_days | formatDateWithMoment }}</td>
-        </tr>
-      </tbody>
-    </table>
-<!-- 개인포인트 표 끝 -->
-<!-- 기업포인트 표 -->
-    <table class="ui celled table" v-if="togglePointTable === 'company'">
-      <thead>
-        <tr>
-          <th>사용/적립일</th>
-          <th>변동양</th>
-          <th>관련 위킨</th>
-          <th>만료일</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr :class="{ negative: history.point_change < 0, positive: history.point_change > 0 }" v-for="history in historys" v-if="!isPeopleType(history.type)">
-          <td>{{ history.created_at | formatDateWithMoment }}</td>
-          <td>{{ history.point_change }}</td>
-          <td >{{ history.WekinNew.ActivityNew.title }}</td>
-          <td>{{ history.due_date_be_written_days | formatDateWithMoment }}</td>
-        </tr>
-      </tbody>
-    </table>
-<!-- 기업포인트 표 끝 -->
+    <div class="ui basic buttons">
+      <div class="ui button" :class="{ active: togglePointTable == 'person' }" @click="togglePointTable='person'">개인포인트</div>
+      <div class="ui button" :class="{ active: togglePointTable !== 'person' }" @click="togglePointTable='company'">기업포인트</div>
+    </div>
+    <!-- 개인포인트 표 -->
+    <div class="table">
+      <table style="border-collapse: collapse;" v-if="togglePointTable === 'person'">
+          <tr>
+            <th class="table-header">타입</th>
+            <th class="table-header">날짜</th>
+            <th class="table-header">변동포인트</th>
+            <th class="table-header">관련 위킨</th>
+            <th class="table-header">만료일</th>
+          </tr>
+          <tr :class="{ negative: history.point_change < 0, positive: history.point_change > 0 }" v-for="history in historys" v-if="isPeopleType(history.type)">
+            <td class="table-body">{{ history.type === 10 ? '적립' : '사용' }}</td>
+            <td class="table-body">{{ history.created_at | formatDateWithMoment }}</td>
+            <td class="table-body">{{ history.point_change }}</td>
+            <td class="table-body">{{ history.WekinNew !== null ? history.WekinNew.ActivityNew.title : '해당없음' }}</td>
+            <td class="table-body">{{ history.due_date_be_written_days | formatDateWithMoment }}</td>
+          </tr>
+      </table>
+    </div>
+    <!-- 개인포인트 표 끝 -->
+    <!-- 기업포인트 표 -->
+    <div class="table">
+      <table style="border-collapse: collapse;" v-if="togglePointTable === 'company'">
+        <thead>
+          <tr>
+            <th class="table-header">타입</th>
+            <th class="table-header">날짜</th>
+            <th class="table-header">변동포인트</th>
+            <th class="table-header">관련 위킨</th>
+            <th class="table-header">만료일</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr :class="{ negative: history.point_change < 0, positive: history.point_change > 0 }" v-for="history in historys" v-if="!isPeopleType(history.type)">
+            <td class="table-body">{{ history.type === 20 ? '적립' : '사용' }}</td>
+            <td class="table-body">{{ history.created_at | formatDateWithMoment }}</td>
+            <td class="table-body">{{ history.point_change }}</td>
+            <td class="table-body">{{ history.WekinNew !== null ? history.WekinNew.ActivityNew.title : '해당없음'}}</td>
+            <td class="table-body">{{ history.due_date_be_written_days | formatDateWithMoment }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- 기업포인트 표 끝 -->
   </div>
 </template>
 <script>
@@ -59,6 +63,7 @@ export default {
     return {
       month: 1,
       historys: null,
+      allHistorys: null,
       togglePointTable: 'person',
     }
   },
@@ -75,7 +80,8 @@ export default {
           results.data.sort(function (a, b) {
             return -moment(a.created_at) + moment(b.created_at)
           })
-          this.historys = results.data
+          this.allHistorys = results.data
+          this.historys = results.data.slice(0, 9)
         })
         .catch( error => window.alert("에러 발생" + error + new Date() + "위킨 카카오톡으로 문의 바랍니다. 죄송합니다."))
     }
@@ -86,4 +92,20 @@ export default {
 }
 </script>
 <style>
+.table {
+  overflow: auto;
+}
+.table-header {
+  padding: 5px 11.7px!important;
+  text-align: center!important;
+  min-width: 100px;
+  max-width: 100px;
+  border-bottom: 1px solid #ddd;
+  border-top: 1px solid #ddd;
+}
+.table-body {
+  text-align: center!important;
+  padding: 5px 11.7px!important;
+  border-bottom: 1px solid #ddd;
+}
 </style>
