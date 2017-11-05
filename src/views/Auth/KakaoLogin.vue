e8e5fe9fa30f7db2f2c5b2f95255869ea7164c39
yusunglee2074 <lys0333@gmail.com>
2017-11-06 01:02:43 +0900

    소셜 로그인 로직 리펙토링

--- src/views/Auth/KakaoLogin.vue
+++ src/views/Auth/KakaoLogin.vue
@@ -1,16 +1,9 @@
 <template>
   <div id="kakaologin">
     <div class="navbar-custom"></div>
-    <div>
-      <div class="ui segment" style="margin-top: 300px;">
         <div class="ui active inverted dimmer">
           <div class="ui large text loader">잠시만 기다려주세요.</div>
         </div>
-        <p></p>
-        <p></p>
-        <p></p>
-      </div>
-    </div>
   </div>
 </template>
 <script>
@@ -32,17 +25,16 @@ export default {
       .then( res => {
         let customToken = res.data.customToken
         let userInfo = res.data.userInfo
-        firebase.auth().signInWithCustomToken(customToken)
-          .then( result => {
-            // 파베 회원가입 완료, 아이디 토큰 얻어서 서버로 보낸다음 서버에서는 유저 정보 업데이트, 디비 생성한다ㅣ.
-            let idToken =  result.De || localStorage.getItem('accessToken')
-            api.dbCreateWithIdToken(idToken)
-              .then( result => {
-                window.location.href = '/'
-              })
-              .catch( error => console.log(error))
-          })
-          .catch( error => console.log(error))
+        return firebase.auth().signInWithCustomToken(customToken)
+      })
+      .then( result => {
+        return firebase.auth().currentUser.getIdToken()
+      })
+      .then(idToken => {
+        return api.dbCreateWithIdToken(idToken)
+      })
+      .then(result => {
+        this.$router.push('/')
       })
       .catch( error => console.log(error))
   }
--- src/views/Auth/NaverLogin.vue
+++ src/views/Auth/NaverLogin.vue
@@ -2,17 +2,13 @@
   <div id="naverlogin">
     <div class="navbar-custom"></div>
     <div>
-      <div class="ui segment" style="margin-top: 300px;">
-        <div class="ui active inverted dimmer">
-          <div class="ui large text loader">잠시만 기다려주세요.</div>
-        </div>
-        <p></p>
-        <p></p>
-        <p></p>
+      <div class="ui active inverted dimmer">
+        <div class="ui large text loader">잠시만 기다려주세요.</div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
 import moment from 'moment'
 import api from './../../api'
@@ -29,25 +25,21 @@ export default {
   },
   mounted() {
     api.kakaoLogin(this.$route.query.code, 'naver')
-      .then( res => {
-        console.log(res)
+      .then(res => {
         let customToken = res.data.customToken
         let userInfo = res.data.userInfo
-        firebase.auth().signInWithCustomToken(customToken)
-          .then( result => {
-            // 파베 회원가입 완료, 아이디 토큰 얻어서 서버로 보낸다음 서버에서는 유저 정보 업데이트, 디비 생성한다ㅣ.
-            let idToken = localStorage.getItem('accessToken') || result.De
-            api.dbCreateWithIdToken(idToken)
-              .then( result => {
-                window.location.href = '/'
-              })
-              .catch( error => console.log(error))
-          })
-          .catch( error => console.log(error))
+        return firebase.auth().signInWithCustomToken(customToken)
       })
-      .catch( error => {
-        console.log(error)
+      .then(result => {
+        return firebase.auth().currentUser.getIdToken()
       })
+      .then(idToken => {
+        return api.dbCreateWithIdToken(idToken)
+      })
+      .then(result => {
+        window.location.href = '/'
+      })
+      .catch(error => console.log(error))
   }
 }
 </script>
