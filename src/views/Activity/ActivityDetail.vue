816761cbe6153266a39db8b995701d0f81d38025
yusunglee2074 <lys0333@gmail.com>
2017-11-24 17:38:20 +0900

    주소 안보이는 문제 해결

--- src/views/Activity/ActivityDetail.vue
+++ src/views/Activity/ActivityDetail.vue
@@ -17,8 +17,8 @@
         <div class="ui sticky">
           <div class="ui segment">
             <h2 style="margin-top:0">{{activity.title}}</h2>
-            <p v-if="activity.address_detail.text">
-              <i class="icon marker"></i>{{activity.address_detail.text}}
+            <p v-if="activity.address">
+              <i class="icon marker"></i>{{activity.address}}
             </p>
             <p>
               <i class="icon won"></i>{{activity.base_price}}
