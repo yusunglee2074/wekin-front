46b1b80208afba8c8db9bf938cbea80822349ed6
yusunglee2074 <lys0333@gmail.com>
2018-01-04 16:54:11 +0900

    회원가입 push 오류 해결

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -60,7 +60,7 @@ export default {
   methods: {
     callSignupModal () {
       this.$emit('update:show', false)
-      this.$router.push('join')
+      this.$router.push('/join')
       // 위에 부모 데이터 조작하기.
       //this.$parent.$parent.$refs.navbar.showSignupModalandCloseLoginModal()
     },
