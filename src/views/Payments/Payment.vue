062726874e828a3200ae5cb288bbe19784a1a3c2
yusunglee2074 <lys0333@gmail.com>
2018-01-30 15:36:14 +0900

    오타수정

--- src/views/Payments/Payment.vue
+++ src/views/Payments/Payment.vue
@@ -356,7 +356,7 @@ export default {
         alert("환불 정보를 확인해주세요.")
       } else if (this.point.value > this.activity.price * this.$route.params.peopleCount) {
         alert("포인트 사용 에러입니다. 포인트 양을 확인해주세요.")
-      } else if (this.phoneValid) {
+      } else if (!this.requestUser.phoneValid) {
         alert("핸드폰이 인증되있지 않습니다. 화면 왼쪽에서 핸드폰 인증을 진행해주세요")
       } else {
         return true
