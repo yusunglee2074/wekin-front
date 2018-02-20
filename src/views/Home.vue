c565d48b810e2db542d457332553ce7eb2a1ca38
yusunglee2074 <lys0333@gmail.com>
2018-02-21 00:04:53 +0900

    externalLink 로 이벤트 배너 뺀다

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
           </div>
           <div v-else>
             <div v-if="banner.externalLink">
-              <a :href="'activity/' + banner.externalLink" target="_blank"><div class="swiper-slide" v-bind:style="{ backgroundImage: `url(${banner.url})`, backgroundSize:'cover', backgroundPosition: 'center' }"></div></a>
+              <a :href="banner.externalLink"><div class="swiper-slide" v-bind:style="{ backgroundImage: `url(${banner.url})`, backgroundSize:'cover', backgroundPosition: 'center' }"></div></a>
             </div>
             <div v-else>
               <a :href="`event/${banner.order}`"><div class="swiper-slide" v-bind:style="{ backgroundImage: `url(${banner.url})`, backgroundSize:'cover', backgroundPosition: 'center' }"></div></a>
