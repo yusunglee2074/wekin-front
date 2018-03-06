717bb66d7bf7aff7f029dcd5d9a3cff58fc497ea
yusunglee2074 <lys0333@gmail.com>
2018-03-06 11:20:55 +0900

    날짜수정 페이지 initialize

---
+++ src/views/HostAdmin/ModifyTime.vue
@@ -0,0 +1,34 @@
+<template>
+</template>
+<script>
+import FireUpload from 'components/FireUpload.vue'
+import api from 'api'
+import auth from './../../auth'
+import moment from 'moment'
+import {Storage} from 'src/util'
+import Datepicker from 'vuejs-datepicker'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  computed: {
+    user() {
+      return this.$store.state.user
+    },
+  },
+  components: {
+    Datepicker
+  },
+  methods: {
+  },
+  created() {
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+</style>
