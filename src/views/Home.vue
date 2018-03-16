b0ec76b16fbc9db8cb7e90cc2800d95d717d5c81
yusunglee2074 <lys0333@gmail.com>
2018-03-16 11:14:10 +0900

    반려견 배너 누르면 바로 엑티비티 디테일로 들어감

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
           </div>
           <div v-else>
             <div v-if="banner.externalLink">
-              <a :href="banner.externalLink"><div class="swiper-slide" v-bind:style="{ backgroundImage: `url(${banner.url})`, backgroundSize:'cover', backgroundPosition: 'center' }"></div></a>
+              <a :href="'activity/' + banner.externalLink"><div class="swiper-slide" v-bind:style="{ backgroundImage: `url(${banner.url})`, backgroundSize:'cover', backgroundPosition: 'center' }"></div></a>
             </div>
             <div v-else>
               <a :href="`event/${banner.order}`"><div class="swiper-slide" v-bind:style="{ backgroundImage: `url(${banner.url})`, backgroundSize:'cover', backgroundPosition: 'center' }"></div></a>
